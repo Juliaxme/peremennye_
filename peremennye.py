e4a4342037feb0bcf41158c2ada1f57f898c58d5
Juliaxme <liasingingheart@gmail.com>
2024-11-29 19:47:34 +0300

    Add files via upload

---
+++ peremennye.py
@@ -0,0 +1,7 @@
+# task peremennye
+
+number_of_homework_tasks = 12
+amount_of_spent_hours = 1.5
+course_name = 'Python'
+time_for_one_task = 0.125
+print('Курс:', course_name, ', всего задач:', number_of_homework_tasks, ', затрачено часов:', amount_of_spent_hours, ', среднее время выполнения:', time_for_one_task, 'часа.')
